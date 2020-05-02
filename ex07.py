8192bc9689594cca62173076434af6dec7ab1c46
Cezar Leão <cezar_guerreiro@users.noreply.github.com>
2020-05-01 23:43:59 -0300

    ex07
    Area do trapezio

---
+++ ex07.py
@@ -0,0 +1,7 @@
+print("Calcula area do trapazio")
+
+base_maior = float(input("Base maior: "))
+base_menor = float(input("Base menor: "))
+altura = float(input("Altura: "))
+
+print("Area: {}".format(((base_maior+base_menor)*altura)/2))
