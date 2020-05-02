2b3a5976529c337782533b8804eb9e78fa7874ec
Cezar Leão <cezar_guerreiro@users.noreply.github.com>
2020-05-01 23:47:12 -0300

    ex08
    Area losango

---
+++ ex08.py
@@ -0,0 +1,6 @@
+print("Calcula area do losango")
+
+diagonal_maior = float(input("Diagonal Maior: "))
+diagonal_menor = float(input("Diagonal Menor: "))
+
+print("Area: {}".format((diagonal_maior * diagonal_menor)/2))
