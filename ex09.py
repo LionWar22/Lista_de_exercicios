af46feb10c07904c6737a4519398a4347ee00e70
Cezar Leão <cezar_guerreiro@users.noreply.github.com>
2020-05-01 23:55:14 -0300

    ex09
    Area circulo

---
+++ ex09.py
@@ -0,0 +1,5 @@
+print("Calcula area circulo")
+
+raio = float(input("Raio: "))
+
+print("Area: {}".format(3.14 * (pow(raio,2)))) #O ENUNCIADO CONTEM UM ERRO, A FORMULA NÃO DIVIDE POR 2 -> A = PI * R^2
