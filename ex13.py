033cf2f920a68a7bc420fda2f4add71d162f68d1
Cezar Leão <cezar_guerreiro@users.noreply.github.com>
2020-05-04 02:23:21 -0300

    ex13

--- ex13.py
+++ ex13.py
@@ -2,13 +2,13 @@
 cadernos = 7
 canetas = 2
 regua = 1
-calculadora = 5.50
+calculadora = 1
 
 caderno_valor = 1.50
 caneta_valor = 0.25
 regua_valor = 0.15
 calculadora_valor = 5.50
 
-valor_total = ((cadernos * caderno_valor) + (canetas * caneta_valor) + (regua * regua_valor) + (calculadora * caneta_valor))
+valor_total = ((cadernos * caderno_valor) + (canetas * caneta_valor) + (regua * regua_valor) + (calculadora * calculadora_valor))
 
 print(valor_total)
