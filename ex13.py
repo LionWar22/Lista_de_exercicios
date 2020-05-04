5249faabbe14db029a7af627e50921622c49ca32
Cezar Leão <cezar_guerreiro@users.noreply.github.com>
2020-05-04 02:21:06 -0300

    ex13

---
+++ ex13.py
@@ -0,0 +1,14 @@
+
+cadernos = 7
+canetas = 2
+regua = 1
+calculadora = 5.50
+
+caderno_valor = 1.50
+caneta_valor = 0.25
+regua_valor = 0.15
+calculadora_valor = 5.50
+
+valor_total = ((cadernos * caderno_valor) + (canetas * caneta_valor) + (regua * regua_valor) + (calculadora * caneta_valor))
+
+print(valor_total)
